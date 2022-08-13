d6e2b25c2ddcac3d89b9c96fc7790d8f6c2aa58c
rimianju <111164183+rimianju@users.noreply.github.com>
2022-08-12 23:48:59 -0300

    Upload do site da Dona Rita

---
+++ lib.php
@@ -0,0 +1,18 @@
+<?php
+
+function comida(){
+
+    $comida = array(
+        array("id" => "0", "nome" => "Frango", "tamanho" => "Pequeno", "preco" => "R$ 12,00" , "imagem" => 'imagem/frango.jpg', 'ingredientes' => 'Todas as Marmitas vão com: Arroz, Feijão e Farofa'),
+        array("id" => "1", "nome" => "Carne", "tamanho" => "Pequeno", "preco" => "R$ 18,00" , "imagem" => 'imagem/carne.jpg', 'ingredientes' => 'Todas as Marmitas vão com: Arroz, Feijão e Farofa'),
+        array("id" => "2","nome" => "Carré", "tamanho" => "Pequeno", "preco" => "R$ 12,00" , "imagem" => 'imagem/carre.jpg', 'ingredientes' => 'Todas as Marmitas vão com: Arroz, Feijão e Farofa'),
+        array("id" => "3","nome" => "Vegana", "tamanho" => "Pequeno", "preco" => "R$ 16,00" , "imagem" => 'imagem/vegana.jpg', 'ingredientes' => 'Todas as Marmitas vão com: Arroz, Feijão e Farofa'),
+        array("id" => "4","nome" => "Frango", "tamanho" => "Média", "preco" => "R$ 12,00" , "imagem" => 'imagem/frango.jpg', 'ingredientes' => 'Todas as Marmitas vão com: Arroz, Feijão e Farofa'),
+        array("id" => "5","nome" => "Carne", "tamanho" => "Média", "preco" => "R$ 18,00" , "imagem" => 'imagem/carne.jpg', 'ingredientes' => 'Todas as Marmitas vão com: Arroz, Feijão e Farofa'),
+        array("id" => "6","nome" => "Carré", "tamanho" => "Média", "preco" => "R$ 12,00" , "imagem" => 'imagem/carre.jpg', 'ingredientes' => 'Todas as Marmitas vão com: Arroz, Feijão e Farofa'),
+        array("id" => "7","nome" => "Vegana", "tamanho" => "Média", "preco" => "R$ 16,00" , "imagem" => 'imagem/vegana.jpg', 'ingredientes' => 'Todas as Marmitas vão com: Arroz, Feijão e Farofa'),
+    );
+
+    return $comida;
+}
+?>
---
+++ marmita.php
@@ -0,0 +1,118 @@
+<?php
+
+include('lib.php');
+
+$id = $_GET['id'];
+
+$comida = comida();
+
+
+
+    
+
+
+
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Marmitas Dona Dirce</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+</head>
+<body>';
+
+echo'<div class="container-fluid">
+<nav style="background-color: #db6868!important;" class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="#"><img src="imagem/logo_marmita.png"  width="50" height="50"></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNavDropdown">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Blog</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Conhece Dona Dirce</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#">Contato</a>
+  </li>
+  </ul>
+</div>
+</nav>';
+foreach($comida as $comidas) {
+    if($comidas['id'] == $id){
+
+        echo '<div class="row">
+        <div class="col"><div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="'.$comidas['imagem'].'" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">'.$comidas['nome'].'</h5>
+        <p class="card-text">'.$comidas['ingredientes'].'</p>
+        <span>Tamanho: </span><p class="card-text">'.$comidas['tamanho'].'</p>
+        <span>Preço: </span><p class="card-text">'.$comidas['preco'].'</p>
+        </div>
+        <a href="index.php" class="btn btn-primary">Voltar</a>
+        </div></div>
+        
+        <div class="col">
+            <p>Dona Dirce<p>
+            <p>End: Rua das Flores, N°0</p>
+            <p>TEL: (21) 9999-99999</p>
+            
+        </div>
+        </div>';
+    }
+}
+
+echo '<footer class="bg-light text-center text-lg-start">
+  <div class="container p-4">
+    <div class="row">
+
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Dedicação</h5>
+
+        <p>
+          Temos anos de experiência em levar o que tem de melhor em quentinhas para sua casa
+        </p>
+      </div>
+
+
+
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Experiência</h5>
+
+        <p>
+           Nossas quentinhas são feitas com qualidade e carinho de quem ama o que faz!
+        </p>
+      </div>
+
+    </div>
+
+  </div>
+ 
+
+
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+   Site desenvolvido por: Raquel Santos
+  </div>
+
+</footer>
+
+</div>
+
+</body>
+</html>';
+
+?>
